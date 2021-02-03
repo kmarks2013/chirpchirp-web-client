a7a15045e25c1273a86e992f39ef77a11910a47c
kmarks2013 <kmarks2013@gmail.com>
2021-02-03 00:37:18 -0500

    adds contianers folder and main ocntainer file with basic text render

---
+++ src/containers/MainContainer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function MainContainer() {
+    return (
+        <div>
+            <p> I will reneder pretty much all content!</p>
+            <p>test of the new render</p>
+        </div>
+    )
+}
+
+export default MainContainer
